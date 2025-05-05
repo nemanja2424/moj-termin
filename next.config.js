48b935583003d0753a9f3b4201807662481085c1
nemanja2424 <njakovlje@gmail.com>
2025-05-05 19:05:32 +0200

    export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images:{
-    unoptimized: true
   },
 };
 
