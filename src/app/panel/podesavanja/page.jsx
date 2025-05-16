95fb95f1c54b57750f7df6b6e67396a86e61c131
nemanja2424 <njakovlje@gmail.com>
2025-05-16 18:29:23 +0200

    /api/ izmena

--- src/app/panel/podesavanja/page.jsx
+++ src/app/panel/podesavanja/page.jsx
@@ -175,7 +175,7 @@ export default function PodesavanjaPage() {
         formData.append('authToken', authToken);
     
         try {
-            const response = await fetch('http://127.0.0.1:5000/api/novi_logo', {
+            const response = await fetch('https://mojtermin.site/api/novi_logo', {
                 method: 'POST',
                 body: formData,
             });
